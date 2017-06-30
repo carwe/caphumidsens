fb932a8f40f7f3a27d817b7e26fb0fe6d0b86635
carwe <info@carwe.de>
2017-06-30 07:39:09 +0200

    add built firmware as example

--- code/make.sh
+++ code/make.sh
@@ -7,9 +7,7 @@ LDFLAGS=
 avr-gcc ${CFLAGS}  -mmcu=${MCU} -c main.c
 avr-gcc ${LDFLAGS} -mmcu=${MCU} main.o -o main.elf
 avr-objcopy -j .text -j .data -O ihex main.elf main.hex
-
+rm main.o main.elf
 micronucleus main.hex
 
 ##avrdude -c dragon_isp -p t85
-
-rm main.o main.elf
