d98826539023c15de98e34d60a291f79d27781f1
carwe <info@carwe.de>
2017-06-30 08:00:56 +0200

    add disassemble cap. to buildscript

--- code/make.sh
+++ code/make.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
+TARGET=caphumidsens
 MCU=attiny85
 CFLAGS=-Os
 LDFLAGS=
 
+
+## BUILD
 avr-gcc ${CFLAGS}  -mmcu=${MCU} -c main.c
-avr-gcc ${LDFLAGS} -mmcu=${MCU} main.o -o main.elf
-avr-objcopy -j .text -j .data -O ihex main.elf main.hex
-rm main.o main.elf
-micronucleus main.hex
+avr-gcc ${LDFLAGS} -mmcu=${MCU} main.o -o ${TARGET}.elf
+avr-objcopy -j .text -j .data -O ihex ${TARGET}.elf ${TARGET}.hex
+avr-objdump -d ${TARGET}.elf > ${TARGET}.disassembly
+
+## CLEANUP
+rm *.o ${TARGET}.elf
+
+
+## PROGRAM
+micronucleus ${TARGET}.hex
 
 ##avrdude -c dragon_isp -p t85
